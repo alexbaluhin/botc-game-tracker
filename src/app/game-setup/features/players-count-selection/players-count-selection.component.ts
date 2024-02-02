885612e33feed3ec576707d1a9b70c7e8e38b825
Alex <baluhins@gmail.com>
2024-02-02 13:02:10 +0200

    feat: add players count selection component

---
+++ src/app/game-setup/features/players-count-selection/players-count-selection.component.ts
@@ -0,0 +1,51 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { ActionBarComponent } from '../../../shared/components/action-bar/action-bar.component';
+import { CharacterTokenComponent } from '../../../shared/components/character-token/character-token.component';
+import { GameSetupInfoService } from '../../data-access/game-setup-info.service';
+import { CounterComponent } from '../../ui/counter/counter.component';
+import { GameSetupHeaderComponent } from '../../ui/game-setup-header/game-setup-header.component';
+
+interface Player {
+  name?: string;
+}
+
+@Component({
+  selector: 'app-players-count-selection',
+  standalone: true,
+  imports: [
+    CommonModule,
+    CharacterTokenComponent,
+    GameSetupHeaderComponent,
+    ActionBarComponent,
+    MatButtonModule,
+    CounterComponent,
+    FormsModule,
+  ],
+  templateUrl: './players-count-selection.component.html',
+  styleUrls: ['./players-count-selection.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PlayersCountSelectionComponent {
+  router = inject(Router);
+  gameSetupInfoService = inject(GameSetupInfoService);
+  playersCount = 12;
+  players: Player[] = new Array(this.playersCount).fill({});
+
+  onPlayersCountChange(newCount: number) {
+    this.playersCount = newCount;
+    if (this.players.length < this.playersCount) {
+      this.players.push({});
+    } else {
+      this.players.pop();
+    }
+  }
+
+  acceptPlayersCount() {
+    this.gameSetupInfoService.setPlayersCount(this.playersCount);
+    this.router.navigate(['characters-selection']);
+  }
+}
