23e556e738c9570faf0b3a5f50567220bfeaba87
Alex <baluhins@gmail.com>
2024-02-02 13:02:30 +0200

    feat: add script selection component

---
+++ src/app/game-setup/features/script-selection/script-selection.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScriptSelectionComponent } from './script-selection.component';
+
+describe('ScriptSelectionComponent', () => {
+  let component: ScriptSelectionComponent;
+  let fixture: ComponentFixture<ScriptSelectionComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ScriptSelectionComponent]
+    });
+    fixture = TestBed.createComponent(ScriptSelectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/game-setup/features/script-selection/script-selection.component.ts
@@ -0,0 +1,62 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Script } from '../../../constants';
+import { CustomScriptJson, CustomScriptJsonMetaItem } from '../../../typings';
+import { GameSetupInfoService } from '../../data-access/game-setup-info.service';
+
+@Component({
+  selector: 'app-script-selection',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage, RouterLink],
+  templateUrl: './script-selection.component.html',
+  styleUrls: ['./script-selection.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ScriptSelectionComponent {
+  router = inject(Router);
+  gameSetupInfoService = inject(GameSetupInfoService);
+
+  scripts = Script;
+
+  onScriptChoose(script: Script) {
+    this.gameSetupInfoService.setScript(script);
+  }
+
+  onCustomScriptLoad(event: Event) {
+    const files = (event.target as HTMLInputElement).files;
+    if (!files) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.readAsText(files[0]);
+    fileReader.onload = () => {
+      try {
+        if (typeof fileReader.result !== 'string') {
+          console.error('Incorrect format of custom script');
+          return;
+        }
+        const customScript = JSON.parse(fileReader.result) as CustomScriptJson;
+        const customScriptMetaInfo = customScript.find(
+          (item): item is CustomScriptJsonMetaItem => item.id === '_meta'
+        );
+        const customScriptCharacters = customScript
+          .filter(({ id }) => id !== '_meta')
+          .map(character => ({
+            ...character,
+            id: character.id.replace(/[_-]/g, ''),
+          }));
+
+        this.gameSetupInfoService.setScript(
+          Script.CUSTOM,
+          customScriptMetaInfo?.name,
+          customScriptCharacters
+        );
+        this.router.navigate(['players-count-selection']);
+      } catch (e) {
+        console.error('Cannot parse custom script');
+      }
+    };
+  }
+}
