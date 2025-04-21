62a3a518404666f7d04f276d41f257a3e81f1199
Alex <baluhins@gmail.com>
2025-04-21 13:09:35 +0300

    feat: update ScriptSelectionComponent

--- src/app/game-setup/features/script-selection/script-selection.component.ts
+++ src/app/game-setup/features/script-selection/script-selection.component.ts
@@ -1,14 +1,14 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { Script } from '../../../constants';
+import { TransparentButtonComponent } from '../../../shared/components/transparent-button/transparent-button.component';
 import { CustomScriptJson, CustomScriptJsonMetaItem } from '../../../typings';
 import { GameStateService } from '../../../shared/data-access/game-state.service';
-import { GameLogoComponent } from '../../ui/game-logo/game-logo.component';
 
 @Component({
   selector: 'app-script-selection',
-  imports: [NgOptimizedImage, RouterLink, GameLogoComponent],
+  imports: [NgOptimizedImage, TransparentButtonComponent],
   templateUrl: './script-selection.component.html',
   styleUrls: ['./script-selection.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
