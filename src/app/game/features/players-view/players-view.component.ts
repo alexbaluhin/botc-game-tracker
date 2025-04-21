21515445478bea8cad0026ea7b0440a521f867e2
Alex <baluhins@gmail.com>
2025-04-21 15:32:10 +0300

    feat: add characters count per type hint to players mode

--- src/app/game/features/players-view/players-view.component.ts
+++ src/app/game/features/players-view/players-view.component.ts
@@ -1,6 +1,7 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActionBarComponent } from '../../../shared/components/action-bar/action-bar.component';
+import { CharactersCountPerTypeHintComponent } from '../../../shared/components/characters-count-per-type-hint/characters-count-per-type-hint.component';
 import { GrimoireComponent } from '../../../shared/components/grimoire/grimoire.component';
 import { IconButtonComponent } from '../../../shared/components/icon-button/icon-button.component';
 import { GameStateService } from '../../../shared/data-access/game-state.service';
@@ -12,6 +13,7 @@ import { GameStateService } from '../../../shared/data-access/game-state.service
     GrimoireComponent,
     IconButtonComponent,
     NgOptimizedImage,
+    CharactersCountPerTypeHintComponent,
   ],
   templateUrl: './players-view.component.html',
   styleUrl: './players-view.component.scss',
