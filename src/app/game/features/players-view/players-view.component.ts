c2476ef016ac793952289e83d4e02a94082f47af
Alex <baluhins@gmail.com>
2025-04-21 15:56:09 +0300

    feat: extract game nav to GameNavComponent

--- src/app/game/features/players-view/players-view.component.ts
+++ src/app/game/features/players-view/players-view.component.ts
@@ -1,19 +1,17 @@
-import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActionBarComponent } from '../../../shared/components/action-bar/action-bar.component';
 import { CharactersCountPerTypeHintComponent } from '../../../shared/components/characters-count-per-type-hint/characters-count-per-type-hint.component';
 import { GrimoireComponent } from '../../../shared/components/grimoire/grimoire.component';
-import { IconButtonComponent } from '../../../shared/components/icon-button/icon-button.component';
 import { GameStateService } from '../../../shared/data-access/game-state.service';
+import { GameNavComponent } from '../../ui/game-nav/game-nav.component';
 
 @Component({
   selector: 'app-players-view',
   imports: [
     ActionBarComponent,
     GrimoireComponent,
-    IconButtonComponent,
-    NgOptimizedImage,
     CharactersCountPerTypeHintComponent,
+    GameNavComponent,
   ],
   templateUrl: './players-view.component.html',
   styleUrl: './players-view.component.scss',
