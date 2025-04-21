3d07319f3243a93be3a511a2318a7f3522b2df09
Alex <baluhins@gmail.com>
2025-04-21 15:00:23 +0300

    chore: remove GameComponent

--- src/app/game/game-routes.ts
+++ src/app/game/game-routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { PlayersViewComponent } from './features/players-view/players-view.component';
-import { GameComponent } from './game.component';
 
 export const gameRoutes: Routes = [
   {
     path: 'game',
-    component: GameComponent,
     children: [
       {
         path: '',
