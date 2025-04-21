e846647a3f3ba4d1ed888063e08f18332e623e37
Alex <baluhins@gmail.com>
2025-04-21 13:08:21 +0300

    feat: add TransparentButtonComponent

---
+++ src/app/shared/components/transparent-button/transparent-button.component.ts
@@ -0,0 +1,21 @@
+import { NgTemplateOutlet } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-transparent-button',
+  imports: [RouterLink, NgTemplateOutlet],
+  templateUrl: './transparent-button.component.html',
+  styleUrl: './transparent-button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TransparentButtonComponent {
+  link = input<string>();
+  disabled = input<boolean>();
+  buttonClicked = output();
+}
