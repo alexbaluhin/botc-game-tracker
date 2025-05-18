ea1107400fcc20608cc4f57bfec03a4b28a6bc61
Alex <baluhins@gmail.com>
2025-05-18 22:05:23 +0300

    feat(player): add "my seat" view

--- src/app/shared/components/player/player.component.ts
+++ src/app/shared/components/player/player.component.ts
@@ -1,10 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Player } from '../../../typings';
 import { CharacterTokenComponent } from '../character-token/character-token.component';
 
 @Component({
   selector: 'app-player',
-  imports: [CharacterTokenComponent],
+  imports: [CharacterTokenComponent, NgOptimizedImage],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
